de7b63c9fb4d242afc0ef79ffaa5d4bfd2faeed8
Yashaswi Patki <73514183+yashaswip@users.noreply.github.com>
2024-02-12 10:09:59 -0500

    Update features.py

--- aim5005/features.py
+++ aim5005/features.py
@@ -31,7 +31,7 @@ class MinMaxScaler:
         diff_max_min = self.maximum - self.minimum
         
         # TODO: There is a bug here... Look carefully! 
-        return x-self.minimum/(self.maximum-self.minimum)
+        return (x-self.minimum)/(self.maximum-self.minimum)
     
     def fit_transform(self, x:list) -> np.ndarray:
         x = self._check_is_array(x)
