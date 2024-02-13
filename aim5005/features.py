8ec466c218d6df3c9840e12ebe823c997395099e
Yashaswi Patki <73514183+yashaswip@users.noreply.github.com>
2024-02-12 19:52:31 -0500

    Update features.py

--- aim5005/features.py
+++ aim5005/features.py
@@ -50,19 +50,59 @@ class StandardScaler:
         assert isinstance(x, np.ndarray), "Expected the input to be a numpy array"
         return x
         
-    def fit(self, x: np.ndarray) ->  None:
+    def fit(self, x: np.ndarray) -> None:
         x = self._check_is_array(x)
         self.mean = np.mean(x, axis=0)
         self.std = np.std(x, axis=0)
         
-    def transform(self, x: np.ndarray)-> np.ndarray:
+    def transform(self, x: np.ndarray) -> np.ndarray:
         x = self._check_is_array(x)
         standardized = (x - self.mean) / self.std
         return standardized
     
-    def fit_transform(self, x: np.ndarray)-> np.ndarray:
+    def fit_transform(self, x: np.ndarray) -> np.ndarray:
         x = self._check_is_array(x)
         self.fit(x)
         return self.transform(x)
+
+class MinMaxScaler:
+    def __init__(self):
+        self.minimum = None
+        self.maximum = None
+        
+    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
+        """
+        Try to convert x to a np.ndarray if it'a not a np.ndarray and return. If it can't be cast raise an error
+        """
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+        
+    
+    def fit(self, x:np.ndarray) -> None:   
+        x = self._check_is_array(x)
+        self.minimum = x.min(axis=0)
+        self.maximum = x.max(axis=0)
+        
+    def transform(self, x:np.ndarray) -> np.ndarray:
+        """
+        MinMax Scale the given vector
+        """
+        x = self._check_is_array(x)
+        diff_max_min = self.maximum - self.minimum
+        
+        # Handling the case where the denominator might be zero
+        if (diff_max_min == 0).any():
+            raise ValueError("Division by zero encountered")
+        
+        return (x - self.minimum) / diff_max_min
+    
+    def fit_transform(self, x:np.ndarray) -> np.ndarray:
+        x = self._check_is_array(x)
+        self.fit(x)
+        return self.transform(x)
+
 
 
